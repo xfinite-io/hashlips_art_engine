f6256ec4273150b43ceff6e0dbf9daf9f16c990a
mukulrevelin <mukul@xfinite.io>
2022-05-26 14:55:05 +0530

    Add: fix

--- src/thumbnail.js
+++ src/thumbnail.js
@@ -7,26 +7,24 @@ const requiredWidth = 128;
 
 //function to start the thumbnail generation
 async function startThumbnailGeneration(){
-   return new Promise( async (resolve,reject ) => {
-      if(!fs.existsSync(dir)){
-         fs.mkdirSync(dir, { recursive: true });
-      }
-      if(!fs.existsSync(outputDir)){
-         console.log( "Error getting output" );
-      }
-      let files = fs.readdirSync(outputDir);
-      if(!files.length){
-         console.log("Error no files found");
-      }
-      for(let i=0;i<files.length; files++){
-         const file = files[i];
-         const image = await resizeImg(fs.readFileSync(`${outputDir}/${file}`), {
-            width: requiredWidth,
-            height: requiredHeight
-         });
-         fs.writeFileSync(`${dir}/${file}`, image);
-      }
-   })
+   if(!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+   }
+   if(!fs.existsSync(outputDir)){
+      console.log( "Error getting output" );
+   }
+   let files = fs.readdirSync(outputDir);
+   if(!files.length){
+      console.log("Error no files found");
+   }
+   for(file of files){
+      console.log(`converting for image file: ${file}`)
+      const image = await resizeImg(fs.readFileSync(`${outputDir}/${file}`), {
+         width: requiredWidth,
+         height: requiredHeight
+      });
+      fs.writeFileSync(`${dir}/${file}`, image);
+   }
 }
 
 ( () => {
